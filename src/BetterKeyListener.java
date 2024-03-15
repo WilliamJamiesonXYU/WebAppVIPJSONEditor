b58eeb18a399b254094be9c9de695acfc5efc179
William Jamieson <jamiewilliamson@protonmail.com>
2024-03-15 16:32:25 -0400

    Works

---
+++ src/BetterKeyListener.java
@@ -0,0 +1,25 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class BetterKeyListener implements KeyListener {
+    private boolean keysDown[] = new boolean[256];
+
+    public boolean isKeyDown(int key) {
+        return keysDown[key];
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() < 256) keysDown[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() < 256) keysDown[e.getKeyCode()] = false;
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+
+}
